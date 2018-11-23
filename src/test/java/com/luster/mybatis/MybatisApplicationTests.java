31318065649ed38b58765670573765a04d7e6bc6
heguangze <heguangze@ifreecomm.com>
2018-11-23 15:33:12 +0800

    添加测试用例

--- src/test/java/com/luster/mybatis/MybatisApplicationTests.java
+++ src/test/java/com/luster/mybatis/MybatisApplicationTests.java
@@ -1,16 +1,26 @@
 package com.luster.mybatis;
 
+import com.luster.mybatis.pojo.User;
+import com.luster.mybatis.service.UserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = MybatisApplication.class)
 public class MybatisApplicationTests {
 
+    @Autowired
+    private UserService userService;
+
     @Test
-    public void contextLoads() {
+    public void findAllUser() {
+        List<User> users=userService.findAllUser(1,10);
     }
 
 }
